88c05a972aae311079f86959d26e9d27629c117a
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2026-04-22 17:18:02 -0300

    criando primeiros testes com rspec

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ bundle install
 .rspec
 
 -- rspec # roda os testes
+-- rspec spec/caminho_arquivo_spec.rb # executa arquivo especifico
+os arquivos de testes devem conter o sufixo _spec no nome
+
+-- it, specify ou example: diz qual é o teste que sera feito naquele momento, e o nome vai da forma como quem esta criado os teste vai usar
+-- expect: indica qual é o resulta esperado
---
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -0,0 +1,15 @@
+require "calculator"
+
+describe Calculator do
+    it "sum method for 2 numbers" do
+        calc = Calculator.new 
+        result = calc.sum(5,7)
+        expect(result).to eq(12)
+    end
+    
+    specify "sum method for 2 numbers with negative number" do
+        calc = Calculator.new 
+        result = calc.sum(-5,7)
+        expect(result).to eq(2)
+    end
+end
