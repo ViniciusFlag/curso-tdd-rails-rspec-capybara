bcada66f8aa585f0830c26ad39ae3916bb16119f
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2026-04-28 16:06:01 -0300

    testando controller

--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -35,7 +35,8 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
