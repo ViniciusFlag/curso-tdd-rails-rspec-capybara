ea0a649a225644564cbe26a4c471c2b30decff11
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2026-04-28 15:30:40 -0300

    Aprendendo sobre shouda-matchers

--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -35,13 +35,20 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
 
   config.include ActiveSupport::Testing::TimeHelpers
-  
+
   # FactoryBot
   config.include FactoryBot::Syntax::Methods
 
