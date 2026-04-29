efa48a24a93f2a4cade25a581713c5b8b191fab9
Vinicius Bandeira <vinicius.bandeira.goncalves@gmail.com>
2026-04-29 12:29:14 -0300

    Aprendendo sobre metcher include_json

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -3,8 +3,28 @@ require 'rails_helper'
 RSpec.describe "Customers", type: :request do
   describe "GET /customers" do
     it "works! (now write some real specs)" do
-      get customers_index_path
+      get customers_path
       expect(response).to have_http_status(200)
+    end
+
+    it "Index - JSON 200 ok" do
+      customer = create(:customer, id: 1, name: "Fulano", email: "fulano@mail.com")
+      get "/customers.json"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json([
+        id: 1,
+        name: "Fulano",
+        email: "fulano@mail.com",
+      ])
+    end
+
+    it "Show - JSON 200 ok" do
+      customer = create(:customer, id: 1, name: "Fulano", email: "fulano@mail.com")
+      get "/customers/1.json"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(
+        id: 1
+      )
     end
   end
 end
